7d4a6545c0dc97ad81ecd5eb118716d07df0d4d3
eZfoRjK <ezforjk@yandex.ru>
2024-04-28 23:44:31 +0300

    Initial commit

---
+++ src/ConvertToRoman.java
@@ -0,0 +1,35 @@
+class ConvertToRoman {
+    public static String[] romanNumbers = new String[]
+            {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+                    "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
+                    "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+                    "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+                    "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+                    "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
+                    "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"};
+
+    public static boolean isRoman(String numbers) {
+        for (int i = 0; i < romanNumbers.length; i++) {
+            if (numbers.equals(romanNumbers[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int convertToArabian(String roman) {
+        for (int i = 0; i < romanNumbers.length; i++) {
+            if (roman.equals(romanNumbers[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static String convertToRoman(int arabian) throws Exception {
+        if (arabian < 1){
+            throw new Exception("Результат меньше 1");
+        }
+        return romanNumbers[arabian];
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+/**
+ * Супер много костылей, но для меня эта первая серьезная задача, раннее решал задачи сугубо те которые были в книгах,
+ * а тут  прямо эффект вау, при этом было супер интересно и супер позновательно
+ */
+public class Main {
+    public static String calc(String input) throws Exception {
+
+        int num1 = 0;
+        int num2 = 0;
+
+        String num_1 = "";
+        String num_2 = "";
+
+        String result = "";
+
+        boolean isRoman;
+
+        String[] operation = input.split(" ");
+        if (operation.length != 3){
+            throw new Exception("Формат математической операции не удовлетворяет заданию");
+        } //Сделал финт :D
+
+        try {
+            num1 = Integer.parseInt(operation[0]);
+            num2 = Integer.parseInt(operation[2]);
+            isRoman = false;
+
+        } catch (Exception e) {
+            num_1 = operation[0];
+            num_2 = operation[2];
+            isRoman = true;
+        }
+
+        if (isRoman) {
+            num1 = ConvertToRoman.convertToArabian(num_1);
+            num2 = ConvertToRoman.convertToArabian(num_2);
+        }
+
+        if (num1 >= 1 && num1 <= 10 && num2 >= 1 && num2 <= 10) {
+
+            switch (operation[1]) {
+                case "+":
+                    result = String.valueOf(num1 + num2);
+                    break;
+                case "-":
+                    result = String.valueOf(num1 - num2);
+                    break;
+                case "/":
+                    result = String.valueOf(num1 / num2);
+                    break;
+                case "*":
+                    result = String.valueOf(num1 * num2);
+                    break;
+                default:
+                    throw new Exception("т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+            }
+
+        } else throw new Exception("Ввод чисил только от 1 до 10 в однои формате(Арабские либо Римские числа)");
+
+
+        if (isRoman) {
+            result = ConvertToRoman.convertToRoman(Integer.parseInt(result));
+        }
+        return String.valueOf(result);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Введите 2 числа(арабские или римские) и оператор(+, -, /, *) через пробел:");
+        System.out.println(calc(keyboard.nextLine()));
+
+    }
+}
